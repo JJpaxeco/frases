742fa1b488adbb88e9da80c31c64873524e707d5
Josenilton Junior <jjpaxeco@hotmail.com>
2024-11-18 19:59:35 -0400

    2

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:fraeses/home.dart';
+
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}
